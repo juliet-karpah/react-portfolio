6b17dd83acaafca7bbd77bc7777cb5ac503285f5
juliet-karpah <122952544+juliet-karpah@users.noreply.github.com>
2023-09-08 11:25:18 -0700

    Update FeaturedIn.jsx

--- makeup-ecommerce/src/ui/FeaturedIn.jsx
+++ makeup-ecommerce/src/ui/FeaturedIn.jsx
@@ -9,7 +9,7 @@ export default function FeaturedIn() {
     <section>
       <div className="flex m-8 flex-col ">
         <h2 className="text-xl font-medium self-center featured-text"> AS FEATURED IN </h2>
-        <div className="flex justify-around sm:flex-row">
+        <div className="flex justify-around sm:flex-row flex-col">
           {logos.map((l, i) => (
             <img className="md:h-16 logo-img" src={l} key={i} />
           ))}
