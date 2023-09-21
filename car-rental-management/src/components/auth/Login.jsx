ac1d872867b0e2026844c45e3f5cdb0688fe2a06
juliet-karpah <122952544+juliet-karpah@users.noreply.github.com>
2023-09-20 17:12:03 -0700

    update login form

--- car-rental-management/src/components/auth/Login.jsx
+++ car-rental-management/src/components/auth/Login.jsx
@@ -1,36 +1,35 @@
 import useLogin from "../../hooks/useLogin";
-import { Button } from "../ui/Button";
+import { ButtonInit } from "../ui/Button";
 import { Form } from "../ui/Form";
 import { H1 } from "../ui/H1";
 import Input from "../ui/Input";
+import { email, password } from "../../services/config";
 
 export default function LoginForm() {
   const { login, isLoading } = useLogin();
 
   const handleSubmit = (error) => {
     error.preventDefault();
-    const email = "juliet@example.com";
-    const password = "test123";
     login({ email, password });
   };
   return (
     <Form width={"300px"} onSubmit={handleSubmit}>
       <H1> Login as Admin </H1>
       <Input
-        placeholder="email"
+        value={email}
         type="email"
         label="Email"
         disabled={isLoading}
       />
       <Input
-        placeholder="password"
+        value={password}
         type="password"
         label="Password"
         disabled={isLoading}
       />
-      <Button type="submit" primary={true}>
+      <ButtonInit type="submit" primary={true}>
         {!isLoading ? "Login" : "loading..."}
-      </Button>
+      </ButtonInit>
     </Form>
   );
 }
--- car-rental-management/src/components/ui/Input.jsx
+++ car-rental-management/src/components/ui/Input.jsx
@@ -10,23 +10,22 @@ export const InputEL = styled.input`
   width: 300px;
 `;
 
-
-export const FileInput = styled.input.attrs({type: "file"})`
-border: none;
-padding: 0.5rem;
-border-radius: var(--border-radius-sm);
-background: #f2f3f5;
-width: 300px;
-`
+export const FileInput = styled.input.attrs({ type: "file" })`
+  border: none;
+  padding: 0.5rem;
+  border-radius: var(--border-radius-sm);
+  background: #f2f3f5;
+  width: 300px;
+`;
 const DIV = styled.div`
-margin: 0.5rem 0.1rem;
-`
+  margin: 0.5rem 0.1rem;
+`;
 
-export default function Input({ label, registerFn, type, errorMessage}) {
+export default function Input({ label, registerFn, value, type, errorMessage }) {
   return (
     <DIV>
       <Label> {label} </Label>
-      <InputEL type={type} {...registerFn} />
+      <InputEL type={type} {...registerFn} value={value}/>
       <Error> {errorMessage} </Error>
     </DIV>
   );
